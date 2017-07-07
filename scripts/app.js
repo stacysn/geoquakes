76283de54f5a71477972afc03984caeedfea01e3
stacysn <stacysuen@Stacy-Stace.local>
2017-07-07 15:49:14 -0700

    name appearance on screen

--- scripts/app.js
+++ scripts/app.js
@@ -4,5 +4,25 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+  $.ajax({
+    method: "GET",
+    url: "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson",
+    dataType: 'json',
+    success: onSuccess
 
+  });
+
+  //
+  // $('#map').append(function onSuccess(responseData)){
+  //   return responseData;
+  // })
 });
+
+
+
+function onSuccess(responseData){
+  console.log("TEST", responseData);
+  var random = responseData.features[0].properties.title;
+  console.log("TEST 2", responseData.features[0].properties.title);
+  $('#info').append(random);
+}
