5d3502f51b1e588f182bdadc501164b225f2e0dd
stacysn <stacysuen@Stacys-MBP.attlocal.net>
2017-07-08 22:33:18 -0700

    added point only on SF

--- scripts/app.js
+++ scripts/app.js
@@ -4,33 +4,37 @@ var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/s
 $(document).ready(function() {
   console.log("Let's get coding!");
   // CODE IN HERE!
+  initMap();
   $.ajax({
     method: "GET",
-    url: "https://earthquake.usgs.gov/fdsnws/event/1/query.geojson?starttime=2017-06-30%2000:00:00&endtime=2017-07-07%2023:59:59&minmagnitude=2.5&orderby=time",
+    url: weekly_quakes_endpoint,
     dataType: 'json',
     success: onSuccess
-
   });
-
-  //
   // $('#map').append(function onSuccess(responseData)){
   //   return responseData;
   // })
 });
 
-
-
 function onSuccess(responseData){
   // var random = responseData.features[0].properties.title;
-
-  var quakeArray = responseData.features;
+  let quakeArray = responseData.features;
   quakeArray.forEach(function(quake){
     console.log(quake.properties.title);
     $("#info").append(`<p> ${quake.properties.title}</p>`);
-    // console.log("print me");
-    // console.log("TESTTTTTTTT");
-  })
-  // console.log("TEST 2", responseData.features[0].properties.title);
-  // $('#info').append(random);
-  // console.log("HIII");
+  });
 }
+
+function initMap(){
+  let map = new google.maps.Map(document.getElementById('map'),{
+    center: {lat: 37.7749, lng: -122.4194},
+    zoom:8
+  });
+
+  let marker = new google.maps.Marker({
+    position: {lat: 37.7749, lng: -122.4194} ,
+    map: map
+    // title: "Sanity Check", //do we need this Title? //what's the point of this?
+  });
+
+};
