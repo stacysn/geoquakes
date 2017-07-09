50450e25d3e8bb969d805e0bbda91ce3a3979b45
stacysn <stacysuen@Stacys-MBP.attlocal.net>
2017-07-08 23:34:29 -0700

    working on forEach() for pins

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,9 @@
 // define globals
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
+var map;
 
 $(document).ready(function() {
-  console.log("Let's get coding!");
+  // console.log("Let's get coding!");
   // CODE IN HERE!
   initMap();
   $.ajax({
@@ -16,25 +17,31 @@ $(document).ready(function() {
   // })
 });
 
+// var random = responseData.features[0].properties.title;
 function onSuccess(responseData){
-  // var random = responseData.features[0].properties.title;
   let quakeArray = responseData.features;
+  console.log("Sanity Check");
+
   quakeArray.forEach(function(quake){
-    console.log(quake.properties.title);
-    $("#info").append(`<p> ${quake.properties.title}</p>`);
+    let title = quake.properties.title;
+
+    $("#info").append(`<p> ${title}</p>`);
+
+    var lat = quake.geometry.coordinates[1];
+    var long = quake.geometry.coordinates[0];
+
+    new google.maps.Marker({
+      position: new google.maps.LatLng(lat, long),
+      map: map,
+      title: title //do we need this Title? //what's the point of this?
+    });
+
   });
 }
 
 function initMap(){
-  let map = new google.maps.Map(document.getElementById('map'),{
+    map = new google.maps.Map(document.getElementById('map'),{
     center: {lat: 37.7749, lng: -122.4194},
     zoom:8
   });
-
-  let marker = new google.maps.Marker({
-    position: {lat: 37.7749, lng: -122.4194} ,
-    map: map
-    // title: "Sanity Check", //do we need this Title? //what's the point of this?
-  });
-
 };
